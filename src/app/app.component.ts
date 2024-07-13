e5d7f284cf4ee2f15b5aa7edb9da66bef2040a0e
akapelas <alexanderkapelas@gmail.com>
2024-07-13 17:16:59 +0300

    Fixed Translate Service for en, gr.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,38 @@
-import {HttpClient} from '@angular/common/http';
 import {Component} from '@angular/core';
-import {ActivatedRoute, RouterModule, RouterOutlet} from '@angular/router';
-import {TranslateModule, TranslateService} from '@ngx-translate/core';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {RouterModule, RouterOutlet} from '@angular/router';
+import {TranslateService} from '@ngx-translate/core';
 import {FooterComponent} from './components/footer/footer.component';
 import {HeaderComponent} from './components/header/header.component';
-
-export function HttpLoaderFactory(httpClient: HttpClient) {
-  return new TranslateHttpLoader(httpClient);
-}
+import {lang, LangService} from './services/lang.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    TranslateModule,
+    RouterModule,
     HeaderComponent,
     FooterComponent,
-    RouterModule,
   ],
-  providers: [ActivatedRoute],
+  providers: [
+    LangService,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'milonas-jewelry';
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService, public langService: LangService) {
     translate.addLangs([
       'en',
-      'gr',
+      'el',
     ]);
-    translate.setDefaultLang('gr');
+    translate.setDefaultLang('el');
 
-    const browserLang: string = !!translate.getBrowserLang() ? <string>translate.getBrowserLang() : 'gr';
-    translate.use(browserLang.match(/en|gr/) ? browserLang : 'gr');
+    langService.updateLang(!!translate.getBrowserLang() ? <lang>translate.getBrowserLang() : 'el');
+
+    console.log('app = ' + langService.lang$.getValue());
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,5 @@
-import {HttpClient, provideHttpClient} from '@angular/common/http';
-import {importProvidersFrom} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
-import {RouterModule} from '@angular/router';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {AppComponent, HttpLoaderFactory} from './app/app.component';
-import {routes} from './app/app.routes';
+import {AppComponent} from './app/app.component';
+import {appConfig} from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    importProvidersFrom(TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient],
-      },
-    }), RouterModule.forRoot(routes)),
-  ],
-});
+bootstrapApplication(AppComponent, appConfig);
